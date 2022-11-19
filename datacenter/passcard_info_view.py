00b2a7fbd4a42037b0d0d9adc8cc96c7a4f5cc5b
Denischerov2312 <hshshsdbhshs@gmail.com>
2022-11-19 20:15:46 +0300

    Передал визиты конкретного человека

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,19 +1,53 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
+from django.utils.timezone import localtime
+import datetime
+
+
+def get_duration(visit):
+    if visit.leaved_at is None:
+        return localtime() - visit.entered_at
+    else:
+        return visit.leaved_at - visit.entered_at
+
+def is_visit_long(visit, minutes=60):
+    if visit.leaved_at == None:
+        return False
+    duration = get_duration(visit)
+    if duration.total_seconds() < minutes * 60:
+        return False
+    else:
+        return True
+
+
+def format_duration(duration):
+    seconds = duration.total_seconds()
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    time = datetime.time(hour=int(hours), minute=int(minutes))
+    return time.strftime('%H:%M')
+
+
+def filter_passcard_visits(visits):
+    passcard_visits = []
+    for visit in visits:
+        duration = get_duration(visit)
+        visit_info = {
+                'entered_at': visit.entered_at,
+                'duration': format_duration(duration),
+                'is_strange': is_visit_long(visit)
+            }
+        passcard_visits.append(visit_info)
+    return passcard_visits
 
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.all()[0]
     # Программируем здесь
-
-    this_passcard_visits = [
-        {
-            'entered_at': '11-04-2018',
-            'duration': '25:03',
-            'is_strange': False
-        },
-    ]
+    passcard = Passcard.objects.get(passcode=passcode)
+    visits = Visit.objects.filter(passcard=passcard)
+    this_passcard_visits = filter_passcard_visits(visits)
+    
     context = {
         'passcard': passcard,
         'this_passcard_visits': this_passcard_visits
